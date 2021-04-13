aaf4c3d6f3e88809a9a7b3c5bbeb9bd2a3236bd6
Waxmatuct <alvheim@rambler.ru>
2021-04-14 01:07:25 +0300

    isNotEmpty()

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,25 +7,28 @@
 	<div class="inner">
 
 		@foreach ($posts as $post)
+			
 			<article class="post">
+				
 				<header class="post-header">
 					<span class="post-category"><a href="{{route('getPostsByCategory', $post->category['slug'])}}"><i class="fas fa-layer-group"></i> {{$post->category['title']}}</a></span>
 					<h2 class="post-title"><a href="{{ route('getPost', $post->slug) }}">{{$post->title}}</a></h2>
 				</header>
+				
 				<section class="post-excerpt"><p>{{ Str::words($post->description,30,) }}</p></section>
+				
 				<div class="meta">
 					<span class="post-date">
 						<time><i class="far fa-clock"></i> {{$post->created_at->diffForHumans()}}</time>
 					</span>
-					@if ($post->tags->count() == 0)
-					<span></span>							
-					@else
-					<span class="post-tag">
-						<i class="fas fa-tags"></i>
-						@foreach ($post->tags as $tag)
-							<a href="{{route('getPostsByTag', $tag['slug'])}}">{{ $tag->name }}</a>
-						@endforeach
-					</span>	
+
+					@if ($post->tags->isNotEmpty())
+						<span class="post-tag">
+							<i class="fas fa-tags"></i>
+							@foreach ($post->tags as $tag)
+								<a href="{{route('getPostsByTag', $tag['slug'])}}">{{ $tag->name }}</a>
+							@endforeach
+						</span>	
 					@endif
 
 					@if (Auth::user())
@@ -34,10 +37,13 @@
 						</span>
 					@endif
 				</div>
-			</article>			
+
+			</article>		
+
 		@endforeach
 
 	</div>
+	
 	<div class="pagination-block">
 		{{ $posts->links('vendor.pagination.semantic-ui') }}
 	</div>
