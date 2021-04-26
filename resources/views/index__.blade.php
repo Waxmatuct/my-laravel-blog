d5c694b07e3c87fea9a7627bb0c3ac15af9384b0
Waxmatuct <alvheim@rambler.ru>
2021-04-26 18:13:57 +0000

    иконка edit

--- resources/views/index__.blade.php
+++ resources/views/index__.blade.php
@@ -43,7 +43,11 @@
 
 								@if (Auth::user())
 									<span class="post-tag">
-										<a class="text-blue text-xs mr-1 border-b border-blue-darker transition duration-300 ease-in-out hover:border-blue" href="{{ route('posts.edit', $post) }}">Править</a>
+										<a href="{{ route('posts.edit', $post) }}">
+											<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+												<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+											  </svg>
+										</a>
 									</span>
 								@endif									
 							</div>
