18a701a3513caa7c99e5ab47d75b38d0c0f7a7d6
Waxmatuct <alvheim@rambler.ru>
2021-06-01 17:57:17 +0000

    shares

--- resources/views/blog/post.blade.php
+++ resources/views/blog/post.blade.php
@@ -71,9 +71,10 @@
                     </a>
                 @endif	
             @endif
-            {{-- <script src="https://yastatic.net/es5-shims/0.0.2/es5-shims.min.js"></script>
-            <script src="https://yastatic.net/share2/share.js"></script>
-            <div class="ya-share2" data-services="collections,vkontakte,facebook,odnoklassniki,moimir"></div> --}}
+            <div class="my-5">
+                <script src="https://yastatic.net/share2/share.js"></script>
+                <div class="ya-share2" data-curtain data-size="s" data-services="vkontakte,facebook,odnoklassniki,telegram,twitter"></div>
+            </div>
         </article>
 	</div>
 </main>
