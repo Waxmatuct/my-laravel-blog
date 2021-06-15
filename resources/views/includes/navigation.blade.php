b302b6e43eee8fdd8f46d5704c32562e92b9336e
Waxmatuct <alvheim@rambler.ru>
2021-06-15 21:41:54 +0300

    стили меню

--- resources/views/includes/navigation.blade.php
+++ resources/views/includes/navigation.blade.php
@@ -7,7 +7,7 @@
             <ul class="flex flex-wrap list-none sm:flex-row space-x-0 sm:space-x-7 space-y-3 sm:space-y-0 sm:items-start items-center flex-col">
                 @if ( request()->is('blog*') )
                     <li class="sm:mb-3">
-                        <a class="menu-item {{ request()->is('blog') ? 'active' : null }}" href="{{ url('/blog') }}">Блог</a>
+                        <a class="menu-item {{ request()->is('blog') ? 'active' : null }} uppercase" href="{{ url('/blog') }}">Блог</a>
                     </li>
                     <li class="sm:mb-3">
                         <span class="hidden sm:block text-black dark:text-light">||</span>
@@ -25,10 +25,10 @@
                         </li>                    
                 @else
                     <li class="">
-                        <a class="menu-item {{ request()->is('blog*') ? 'active' : null }}" href="{{ url('/blog') }}">Блог</a>
+                        <a class="menu-item {{ request()->is('blog*') ? 'active' : null }} uppercase" href="{{ url('/blog') }}">Блог</a>
                     </li>
                     <li class="">
-                        <a class="menu-item {{ request()->is('projects') ? 'active' : null }}" href="{{ url('/projects') }}">Проекты</a>
+                        <a class="menu-item {{ request()->is('projects') ? 'active' : null }} uppercase" href="{{ url('/projects') }}">Проекты</a>
                     </li>
                     <li class="">
                         @include('includes.switcher')
