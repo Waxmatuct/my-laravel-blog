60a21dd2cdee558ed66728ba90776dffec477d07
Waxmatuct <alvheim@rambler.ru>
2021-11-10 18:05:43 +0000

    code css

--- resources/views/layouts/blog.blade.php
+++ resources/views/layouts/blog.blade.php
@@ -1,59 +1,63 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<meta name="description" content="">
-		<meta name="author" content="">
-		<link rel="icon" href="{{ asset('favicon.ico') }}">
 
-		<title>@yield('title')</title>
-		<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
-		<link href="https://fonts.googleapis.com/css?family=Roboto:400,400i,700,700i&display=swap&subset=cyrillic" rel="stylesheet">
-		<script>
-			if (localStorage.theme === 'dark') {
-				document.documentElement.classList.add('dark')
-				localStorage.theme = 'dark';
-			} else {
-				document.documentElement.classList.remove('dark');
-				localStorage.removeItem('theme');
-			}
-		</script>
-		@stack('recaptcha')
-	</head>
-	<body class="bg-light text-black font-sans dark:bg-dark-lighter dark:text-gray">
-		<a href="#" onclick="return false" class="menu_link"><span></span></a>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="{{ asset('favicon.ico') }}">
 
-		@include('blog.includes.navigation')
+    <title>@yield('title')</title>
+    <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,400i,700,700i&display=swap&subset=cyrillic"
+        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Fira+Code&display=swap" rel="stylesheet">
+    <script>
+        if (localStorage.theme === 'dark') {
+            document.documentElement.classList.add('dark')
+            localStorage.theme = 'dark';
+        } else {
+            document.documentElement.classList.remove('dark');
+            localStorage.removeItem('theme');
+        }
+    </script>
+    @stack('recaptcha')
+</head>
 
-		<div class="container mx-auto">
+<body class="bg-light text-black font-sans dark:bg-dark-lighter dark:text-gray">
+    <a href="#" onclick="return false" class="menu_link"><span></span></a>
 
-           @yield('content')
+    @include('blog.includes.navigation')
 
-		</div>
+    <div class="container mx-auto">
 
-		@include('includes.footer')
-		
-		<script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
-		<script src="{{ asset('/js/scripts.js') }}"></script>
-		<script>
-			document.getElementById('showNav').addEventListener('click', function() {
+        @yield('content')
 
-				var menu = document.getElementById('menu').classList;
+    </div>
 
-				if (menu.contains('hidden')) {
+    @include('includes.footer')
 
-				menu.remove('hidden');
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+    <script src="{{ asset('/js/scripts.js') }}"></script>
+    <script>
+        document.getElementById('showNav').addEventListener('click', function() {
 
-				} else {
+            var menu = document.getElementById('menu').classList;
 
-				menu.add('hidden');
+            if (menu.contains('hidden')) {
 
-				}
+                menu.remove('hidden');
 
-			});
-		</script>
-		@stack('fade')
-	</body>
-	
+            } else {
+
+                menu.add('hidden');
+
+            }
+
+        });
+    </script>
+    @stack('fade')
+</body>
+
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,63 +1,55 @@
 module.exports = {
+    purge: ["./resources/**/*.blade.php", "./resources/**/*.js"],
+    darkMode: "class", // or 'media' or 'class'
 
-  purge: [
-    './resources/**/*.blade.php',
-    './resources/**/*.js',
+    theme: {
+        extend: {},
 
-  ],
-  darkMode: 'class', // or 'media' or 'class'
-
-  theme: {
-    extend: {},
-
-    fontFamily: {
-      sans: ['Roboto', 'sans-serif'],
-      serif: ['Noto Serif', 'serif'],
-      mono: ['monospace'],
-    },
-
-    colors: {
-      
-      current: 'currentColor',
-      
-      dark: {
-          DEFAULT: '#111827', //#1c1c28
-          lighter: '#161e2d', //#23243a
-          gray: '#777',
+        fontFamily: {
+            sans: ["Roboto", "sans-serif"],
+            serif: ["Noto Serif", "serif"],
+            mono: ["Fira Code", "monospace"],
         },
-      
-      light: {
-        DEFAULT: '#ffffff',
-        white: '#f9f9fa',
-        gray: '#e9e9e9',
-      },
 
-      'black': '#374151',
-      'gray': '#9ca3af',
-      'gray-900': '#1f2937', 
-    
-      'primary': {
-          DEFAULT: '#0ba057',//#98c376
-          darker: '#48bb78',
-      },
-      
+        colors: {
+            current: "currentColor",
+
+            dark: {
+                DEFAULT: "#111827", //#1c1c28
+                lighter: "#161e2d", //#23243a
+                gray: "#777",
+            },
+
+            light: {
+                DEFAULT: "#ffffff",
+                white: "#f9f9fa",
+                gray: "#e9e9e9",
+            },
+
+            black: "#374151",
+            gray: "#9ca3af",
+            "gray-900": "#1f2937",
+
+            primary: {
+                DEFAULT: "#0ba057", //#98c376
+                darker: "#48bb78",
+            },
+        },
+        //
+        borderWidth: {
+            DEFAULT: "1px",
+            0: "0",
+            2: "2px",
+            3: "3px",
+            4: "4px",
+            5: "5px",
+            6: "6px",
+            7: "7px",
+            8: "8px",
+        },
     },
-// 
-    borderWidth: {
-      DEFAULT: '1px',
-          '0': '0',
-          '2': '2px',
-          '3': '3px',
-          '4': '4px',
-          '5': '5px',
-          '6': '6px',
-          '7': '7px',
-          '8': '8px',
+    variants: {
+        extend: {},
     },
-    
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+    plugins: [],
+};
